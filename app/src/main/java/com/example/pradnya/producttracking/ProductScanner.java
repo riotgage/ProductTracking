0e6f9d703b350c7e9965e59dcb108dd313e6a5ff
riotgage <48171184+riotgage@users.noreply.github.com>
2019-07-13 14:28:29 +0530

    Box Scanner and Product Scanner

--- app/src/main/java/com/example/pradnya/producttracking/ProductScanner.java
+++ app/src/main/java/com/example/pradnya/producttracking/ProductScanner.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,11 +25,15 @@ import java.util.Collection;
 import java.util.List;
 
 public class ProductScanner extends AppCompatActivity {
-    TextView box_quant,desc,cat_id;
+    private TextView box_quant,desc,cat_id,prod_count;
     private DecoratedBarcodeView barcodeView;
     private BeepManager beepManager;
     private String text="";
+    private Button prod_scan;
+    static  int count=0;
+
     private ArrayList<String> list;
+    String unique_id,prod_quant,prod_quant_no;
     private BarcodeCallback callback = new BarcodeCallback() {
         @Override
 
@@ -37,9 +42,16 @@ public class ProductScanner extends AppCompatActivity {
                 return;
             }
             text=result.getText();
-            if(list.contains(text)){
-                Toast.makeText(ProductScanner.this, "Product Already Scanned", Toast.LENGTH_SHORT).show();
-
+            String[] splits;
+            splits=text.split("\\|");
+            if(splits.length<7){
+                Toast.makeText(ProductScanner.this, "Something is missing", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            unique_id=splits[0];
+            prod_quant=splits[5];
+            if(!unique_id.equals(Scanner.unique_no)){
+                Toast.makeText(ProductScanner.this, "Unique ID miss match ,Try again", Toast.LENGTH_SHORT).show();
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     vib.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
                 } else {
@@ -48,6 +60,17 @@ public class ProductScanner extends AppCompatActivity {
                 }
                 return;
             }
+            if(list.contains(text)){
+                Toast.makeText(ProductScanner.this, "Product Already Scanned", Toast.LENGTH_SHORT).show();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vib.vibrate(VibrationEffect.createOneShot(500,1));
+                } else {
+                    vib.vibrate(500);
+                }
+                return;
+            }
+            count+=1;
             list.add(text);
             barcodeView.setStatusText(text);
 
@@ -70,7 +93,7 @@ public class ProductScanner extends AppCompatActivity {
         box_quant=findViewById(R.id.box_quant);
         desc=findViewById(R.id.desc);
         cat_id=findViewById(R.id.cat_id);
-
+        prod_count=findViewById(R.id.prod_count);
         box_quant.setText(Scanner.Box_quant);
         desc.setText(Scanner.Desc);
         cat_id.setText(Scanner.cat_no);
@@ -81,7 +104,7 @@ public class ProductScanner extends AppCompatActivity {
         barcodeView.initializeFromIntent(getIntent());
         barcodeView.decodeContinuous(callback);
         barcodeView.setStatusText("Please First Scan .");
-
+        prod_count.setText(Integer.toString(count));
         beepManager = new BeepManager(this);
 
     }
