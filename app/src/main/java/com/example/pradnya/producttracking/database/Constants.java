7c291b9f5cc573993d00d389a6b1c352bc8d1939
riotgage <48171184+riotgage@users.noreply.github.com>
2019-07-16 14:12:35 +0530

    MySql Database

--- app/src/main/java/com/example/pradnya/producttracking/database/Constants.java
+++ app/src/main/java/com/example/pradnya/producttracking/database/Constants.java
@@ -1,6 +1,13 @@
 package com.example.pradnya.producttracking.database;
 
 public class Constants {
+    private static final String ROOT_URL="http://192.168.43.150/ProductTracking/Functions/";
 
+
+    public static  final String URL_Create_Table=ROOT_URL+"CreateTable.php";
+
+    public static  final String URL_Add_Product=ROOT_URL+"AddProduct.php";
+
+    public static  final String URL_CHECK_Product=ROOT_URL+"check_product.php";
 
 }
