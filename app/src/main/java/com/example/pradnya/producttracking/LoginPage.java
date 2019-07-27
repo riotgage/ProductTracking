ab9c0cf41144fbd4bdfa70c392dbf3ec14b4e00f
Prady9 <katariyapradnya0905@gmail.com>
2019-07-27 15:29:49 +0530

    Commit with login & Admin Registration page

---
+++ app/src/main/java/com/example/pradnya/producttracking/LoginPage.java
@@ -0,0 +1,66 @@
+package com.example.pradnya.producttracking;
+
+import android.content.Intent;
+import android.inputmethodservice.KeyboardView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginPage extends AppCompatActivity {
+
+    EditText etextuser;
+    EditText etextpassword;
+    TextView textRegister;
+    TextView textadmin;
+    Button btn_login;
+    DatabaseHelper db;
+    Button go;
+    EditText admin_pass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+        etextuser = (EditText) findViewById(R.id.usrid);
+        etextpassword = (EditText) findViewById(R.id.pass);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        textadmin = (TextView) findViewById(R.id.admin);
+        textRegister = (TextView) findViewById(R.id.reg);
+        go = (Button) findViewById(R.id.btn_go);
+        admin_pass = (EditText) findViewById(R.id.adminpass);
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent registerIntent = new Intent(LoginPage.this, RegisterActivity.class);
+                admin_pass.setVisibility(View.VISIBLE);
+                go.setVisibility(View.VISIBLE);
+                go.setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View v) {
+
+                        if (admin_pass.toString().equals("12345")){
+                            Toast.makeText(getApplication(),"Confirmed as Admin", Toast.LENGTH_SHORT).show();
+                            startActivity(registerIntent);
+                        }
+                        else {
+                            Toast.makeText(getApplication(),"Cannot register Contact Admin", Toast.LENGTH_SHORT).show();
+                        }
+                    }   });
+            }
+        });
+
+
+        }
+
+
+
+
+
+    }
+
